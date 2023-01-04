3fabb60d39178791ae9ca52c31eecd2db0941258
Anna Marie Sterling <sterling.am@gmail.com>
2023-01-03 19:11:38 -0700

    create factories

---
+++ spec/factories/teas.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :tea do
+    title { Faker::Tea.variety }
+    description { Faker::Food.description }
+    temperature { Faker::Number.within(range: 175..205) }
+    brew_time { Faker::Number.within(range: 3..7) }
+  end
+end
