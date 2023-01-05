d68e91013edfee818615ff9af9e5bef9f43eef57
Anna Marie Sterling <sterling.am@gmail.com>
2023-01-05 16:31:05 -0700

    module for responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include Response 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
   rescue_from ArgumentError, with: :unprocessable_entity
---
+++ app/controllers/concerns/response.rb
@@ -0,0 +1,5 @@
+module Response
+  def json_response(object, status = :ok)
+    render json: SubscriptionSerializer.new(object), status: status
+  end
+end
